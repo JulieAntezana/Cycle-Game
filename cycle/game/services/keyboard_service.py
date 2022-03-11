7f40de56a79efa242235dee83cf2d51c902a0f0d
JulieAntezana <jantezana@byui.edu>
2022-03-11 10:11:45 -0600

    Added spacebar to keyboard services

--- cycle/game/services/keyboard_service.py
+++ cycle/game/services/keyboard_service.py
@@ -25,13 +25,13 @@ class KeyboardService:
         self._keys['k'] = pyray.KEY_K
         self._keys['l'] = pyray.KEY_L
 
-        self._keys['r'] = pyray.KEY_R
+        self._keys['space'] = pyray.KEY_SPACE
 
     def is_key_up(self, key):
         """Checks if the given key is currently up.
         
         Args:
-            key (string): The given key (w, a, s, d or i, j, k, l)
+            key (string): The given key (w, a, s, d or i, j, k, l, space)
         """
         pyray_key = self._keys[key.lower()]
         return pyray.is_key_up(pyray_key)
@@ -40,7 +40,7 @@ class KeyboardService:
         """Checks if the given key is currently down.
         
         Args:
-            key (string): The given key (w, a, s, d or i, j, k, l)
+            key (string): The given key (w, a, s, d or i, j, k, l, space)
         """
         pyray_key = self._keys[key.lower()]
         return pyray.is_key_down(pyray_key)
